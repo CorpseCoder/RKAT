1b3578943e5065f6619e042056b61b27b0744e8d
CorpseCoder <68142751+CorpseCoder@users.noreply.github.com>
2025-02-02 10:23:56 +0530

    feat: Implement auto-install for modules

--- RKAT.py
+++ RKAT.py
@@ -1,1 +1,15 @@
-import keyboard,os,requests
+def checkModules():
+    try:
+        import keyboard,os,requests
+        print("Modules installed already")
+    except ModuleNotFoundError as err:
+        if "keyboard" in str(err):
+            os.run("pip3 install keyboard")
+            print("Keyboard module installed")
+            checkModules()
+        elif "requests" in str(err):
+            os.run("pip3 install requests")
+            print("Requests module installed")
+            checkModules()
+
+checkModules()
